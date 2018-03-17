ee674c92b223a8e31b4d78e56fbaceb89d9ffeb5
Dan Aukes <danaukes@gmail.com>
2018-03-17 05:21:51 -0700

    fixed deploy script to address new files

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+mo#!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
 function doCompile {
@@ -36,14 +36,15 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+# THIS DOES NOT WORK WITH NEW FILES
+#if [ -z `git diff --exit-code` ]; then
+#    echo "No changes to the output on this push; exiting."
+#    exit 0
+#fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
